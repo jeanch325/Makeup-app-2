cdab44b63a3f68749e8f2277c23a00a7c6e6ef3d
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-10 16:32:41 -0500

    Parse JSON data

--- Makeup app 2/ProductViewController.swift
+++ Makeup app 2/ProductViewController.swift
@@ -16,7 +16,36 @@ class ProductViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "Makeup Products"
         let query = "https://makeup-api.herokuapp.com/api/v1/products.json"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                parse(json: json)
+                return
+            }
+        }
+        loadError()
     }
+    
+    //Step 3
+    func parse(json : JSON) {
+        for result in json["products"].arrayValue {
+            let product = result["product_type"].stringValue
+            if !products.contains(product) {
+                products.append(product)
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the makeup products",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
